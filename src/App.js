147fdea8a4f6164279acd90cfabdedc069dcc4fe
cchavesdev <cchaves1395dev@gmail.com>
2022-09-10 13:06:10 -0600

    cleaning code and start from scratch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { Link } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Dashboard from './components/Dashboard/Dashboard';
-import Week from './components/Week/Week';
 
 function App() {
   return (
-   <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/dashboard' element={<Dashboard/>}/>
-    <Route path='/week/:id' element={<Week/>}/>
-   </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
