86bfe20e09b9da8f88afd5efc6cf23cc9d251eda
cchavesdev <cchaves1395dev@gmail.com>
2022-09-12 20:05:08 -0600

    Removing local url

--- src/components/Week/Week.js
+++ src/components/Week/Week.js
@@ -14,7 +14,7 @@ function Week(props) {
     function getUserData() {
    
       axios
-        .get(`http://localhost:7055/api/getuserdata/${id}`)
+        .get(`https://home-app-function.azurewebsites.net/api/getuserdata/${id}`)
         .then((response) => {
           setTasks(response.data[0].tasks);
         });
