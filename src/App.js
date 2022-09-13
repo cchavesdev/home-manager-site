d17d284fab559f42da8bfb7c65502a09ff7ae4cc
cchavesdev <cchaves1395dev@gmail.com>
2022-09-12 19:51:13 -0600

    making dashboard as home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
 import Dashboard from './components/Dashboard/Dashboard';
 import Week from './components/Week/Week';
 
 function App() {
   return (
    <Routes>
-    <Route path='/' element={<Home/>}/>
+    <Route path='/' element={<Dashboard/>}/>
     <Route path='/dashboard' element={<Dashboard/>}/>
     <Route path='/week/:id' element={<Week/>}/>
    </Routes>
